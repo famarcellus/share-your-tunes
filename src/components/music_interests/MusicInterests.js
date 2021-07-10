b85f2ef984086ca99effdc953b9fc71e8526ce70
Fabio <famarcellus12@gmail.com>
2021-07-10 11:10:49 -0400

    Add animated music interests and refactor css

--- src/components/music_interests/MusicInterests.js
+++ src/components/music_interests/MusicInterests.js
@@ -24,17 +24,17 @@ function MusicInterests() {
     return (
         <section className="user-music-section">
             <Tabs defaultActiveKey="artists">
-                <TabPane className="artists-section" tab="Artists" key="artists">
+                <TabPane className="artists-section sub-section" tab="Artists" key="artists">
                     {artistList.length !== 0 ? artistList.map((item, index) => {
                         return (<Artist name={item.name} imgSrc={item.src} artistList={artistList} index={index} removeFn={RemoveItem} setArtistList={setArtistList}/>)
                     }) : <h2 className="empty-artists empty">No artists added yet!</h2> }
                 </TabPane>
-                <TabPane className="tracks-section" tab="Tracks" key="tracks">
+                <TabPane className="tracks-section sub-section" tab="Tracks" key="tracks">
                     {trackList.length !== 0 ? trackList.map((item, index) => {
                         return (<Track artist={item.artist} trackName={item.trackName} imgSrc={item.imgSrc} trackList={trackList} index={index} removeFn={RemoveItem} setTrackList={setTrackList}/>)
                     }) : <h2 className="empty-tracks empty">No tracks added yet!</h2> }
                 </TabPane>
-                <TabPane className="albums-section" tab="Albums" key="albums">
+                <TabPane className="albums-section sub-section" tab="Albums" key="albums">
                     {albumList.length !== 0 ? albumList.map((item, index) => {
                         return (<Album artist={item.artist} albumName={item.albumName} imgSrc={item.imgSrc} albumList={albumList} index={index} removeFn={RemoveItem} setAlbumList={setAlbumList}/>)
                     }) : <h2 className="empty-albums empty">No albums added yet!</h2> }
